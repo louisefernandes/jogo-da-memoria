00ca28b845d64f22587cee98add9fd3d9bdca2fa
Louise <louisefcaetano@gmail.com>
2023-09-24 15:06:03 -0300

    jogo finalizado

--- app/src/main/java/com/example/memoria/Jogo.kt
+++ app/src/main/java/com/example/memoria/Jogo.kt
@@ -1,7 +1,9 @@
 package com.example.memoria
 
+import android.widget.ImageView
+
 class Jogo {
-    var numeroMaximoTentativas = 20
+    var numeroMaximoTentativas = 10
     private var tentativasErradas = 0
     var paresEncontrados = 0
     var cartas = mutableListOf<Int>()
@@ -18,7 +20,10 @@ class Jogo {
         )
         this.cartas.shuffle()
     }
-    fun verificarTentativa(primeiraCarta: Int, segundaCarta: Int): Boolean {
+    fun verificarTentativa(cartas: MutableList<ImageView>): Boolean {
+
+        var primeiraCarta = cartas[0].tag as Int
+        var segundaCarta = cartas[1].tag as Int
 
         if (numeroMaximoTentativas > 0) {
            if (primeiraCarta == segundaCarta) {
@@ -32,76 +37,19 @@ class Jogo {
         }
         return false
     }
-    fun verificarFimDoJogo(): Boolean {
-        return paresEncontrados == 8 || numeroMaximoTentativas == 0
-    }
-    fun getTentativasErradas(): Int {
-        return tentativasErradas
+    fun verificarFimDoJogo(): String {
+        if (paresEncontrados == 8) {
+            return "Ganhou!"
+        } else if (numeroMaximoTentativas == 0) {
+            return "Perdeu!"
+        } else {
+            return "Continue jogando"
+        }
     }
     fun novoJogo() {
-        numeroMaximoTentativas = 20
+        numeroMaximoTentativas = 10
         tentativasErradas = 0
         paresEncontrados = 0
         this.cartas.shuffle()
     }
 }
-//private var numeroMaximoTentativas = 10
-//private var tentativasErradas = 0
-//private var paresEncontrados = 0
-//private var primeiraCarta: ImageView? = null
-//private var segundaCarta: ImageView? = null
-
-//constructor() {
-//    this.numeroMaximoTentativas = 10
-//    criarTabuleiro()
-//}
-//private fun criarTabuleiro() {
-//    val totalDeCartas = 8
-//    val nomesDasImagens = mutableListOf<String>()
-//
-//    for (i in 1..totalDeCartas) {
-//        nomesDasImagens.add("imageProf$i")
-//    }
-//
-//    nomesDasImagens.shuffle()
-//}
-//fun virarCarta(carta: ImageView, s: String) {
-//    if (primeiraCarta == null) {
-//        primeiraCarta = carta
-//        carta.visibility = View.VISIBLE
-//    } else if (segundaCarta == null) {
-//        segundaCarta = carta
-//        carta.visibility = View.VISIBLE
-//        verificarPar()
-//    }
-//}
-//fun verificarPar() {
-//    if (primeiraCarta != null && segundaCarta != null) {
-//        val imagemCarta1 = primeiraCarta!!.drawable
-//        val imagemCarta2 = segundaCarta!!.drawable
-//
-//        if (imagemCarta1.constantState == imagemCarta2.constantState) {
-//            registrarParEncontrado()
-//        } else {
-//            registrarTentativaErrada()
-//        }
-//
-//        primeiraCarta = null
-//        segundaCarta = null
-//    }
-//}
-//private fun virarCartaDeVolta(carta: ImageView) {
-//    carta.visibility = View.INVISIBLE
-//}
-//fun registrarTentativaErrada() {
-//    tentativasErradas++
-//}
-//fun getTentativasErradas(): Int {
-//    return tentativasErradas
-//}
-//fun verificarFimDoJogo(): Boolean {
-//    return paresEncontrados == 8 || tentativasErradas >= numeroMaximoTentativas
-//}
-//fun registrarParEncontrado() {
-//    paresEncontrados++
-//}
--- app/src/main/java/com/example/memoria/MainActivity.kt
+++ app/src/main/java/com/example/memoria/MainActivity.kt
@@ -1,17 +1,14 @@
 package com.example.memoria
 
 import android.content.Intent
-import android.opengl.Matrix
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
+import android.os.CountDownTimer
 import android.widget.ImageView
 import android.widget.ProgressBar
 import android.widget.TextView
-import android.widget.Toast
-import android.os.Handler
 import android.widget.GridLayout
-import androidx.core.view.marginBottom
+import androidx.core.view.children
 
 class MainActivity : AppCompatActivity() {
 
@@ -20,6 +17,7 @@ class MainActivity : AppCompatActivity() {
     private lateinit var tvTentativas: TextView
     private lateinit var progressBar1: ProgressBar
     private lateinit var memoria: GridLayout
+    private var listaCartasClicadas = mutableListOf<ImageView>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -34,274 +32,100 @@ class MainActivity : AppCompatActivity() {
         for (carta in jogo.cartas) {
             var imageProf = ImageView(this)
             this.memoria.addView(imageProf)
-            imageProf.layoutParams.height = 250
-            imageProf.layoutParams.width = 250
-            imageProf.setBackgroundColor(R.drawable.ic_launcher_background)
+            imageProf.layoutParams.height = 150
+            imageProf.layoutParams.width = 150
             imageProf.setImageResource(carta)
-            imageProf.imageAlpha = 0 //pesquisar como colocar margem
+            imageProf.tag = carta
+        }
+
+        object : CountDownTimer(5000, 50) {
+            override fun onTick(millisUntilFinished: Long) {
+                progressBar1.progress += 1
+            }
+            override fun onFinish() {
+                virarTodasCartas()
+            }
+        }.start()
+    }
+
+    private fun virarTodasCartas() {
+        for (image in memoria.children) {
+            val novaImagem = image as ImageView
+            novaImagem.setImageResource(R.drawable.ic_launcher_background)
+            image.setOnClickListener({cliqueCarta(image)})
         }
     }
 
-//    private fun esconderCarta() {
-
+    private fun desvirarCarta(cartas: MutableList<ImageView>) {
+        for (carta in cartas) {
+            carta.setImageResource(R.drawable.ic_launcher_background)
+        }
+    }
+    private fun cliqueCarta(carta: ImageView) {
+        carta.setImageResource(carta.tag as Int)
+        this.listaCartasClicadas.add(carta)
+        if (this.listaCartasClicadas.size == 2) {
+            if (!this.jogo.verificarTentativa(this.listaCartasClicadas)) {
+                val cartasClicadas = this.listaCartasClicadas.toMutableList()
+                object : CountDownTimer(1000, 10) {
+                    override fun onTick(millisUntilFinished: Long) {
+                    }
+                    override fun onFinish() {
+                        desvirarCarta(cartasClicadas)
+                    }
+                }.start()
+            }
+            this.listaCartasClicadas.clear()
+            tvTentativas.setText("Tentativas restantes: ${this.jogo.numeroMaximoTentativas}")
+            this.atualizarInterface()
+        }
     }
 
-//    private fun virarCarta(carta: ImageView) {
-//        val imagemResource = jogo.cartas[carta.id]
-//        if (imagemResource != 0) {
-//            carta.setImageResource(imagemResource)
-//        } else {
-//            carta.setImageResource(R.drawable.ic_launcher_background)
-//        }
-//
-//        if (primeiraCartaVirada == null) {
-//            primeiraCartaVirada = carta
-//        } else {
-//            segundaCartaVirada = carta
-//            bloquearToque = true
-//
-//            Handler().postDelayed({
-//                val primeiroId = primeiraCartaVirada!!.id
-//                val segundoId = segundaCartaVirada!!.id
-//
-//                val match = jogo.verificarTentativa(primeiroId, segundoId)
-//
-//                if (!match) {
-//                    primeiraCartaVirada?.setImageResource(R.drawable.ic_launcher_background)
-//                    segundaCartaVirada?.setImageResource(R.drawable.ic_launcher_background)
-//                }
-//
-//                primeiraCartaVirada = null
-//                segundaCartaVirada = null
-//                bloquearToque = false
-//
-//                if (jogo.verificarFimDoJogo()) {
-//                    exibirMensagemFimDoJogo()
-//                }
-//            }, 1000)
-//        }
-//    }
-//
-//    private fun progress() {
-//        Thread {
-//            while (this.progressBar1.progress < 100) {
-//                this.progressBar1.progress += 1
-//                Thread.sleep(100)
-//            }
-//            runOnUiThread {
-//                this.progressBar1.visibility = View.INVISIBLE
-//            }
-//        }.start()
-//    }
-//
-//    private fun exibirMensagemFimDoJogo() {
-//        val mensagem = if (jogo.paresEncontrados == jogo.cartas.size / 2) {
-//            "Você ganhou!"
-//        } else {
-//            "Fim de jogo! Você perdeu."
-//        }
-//
-//        Toast.makeText(this, mensagem, Toast.LENGTH_SHORT).show()
-//
-//        if (jogo.paresEncontrados == jogo.cartas.size / 2) {
-//            indoTelaGanhou()
-//        } else {
-//            indoTelaPerdeu()
-//        }
-//    }
-//    private fun novoJogo() {
-//        val imagensEmbaralhadas = criarListaDeImagens().shuffled()
-//        jogo.novoJogo()
-//        tvTentativas.text = "Tentativas: ${jogo.numeroMaximoTentativas}"
-//        progressBar1.progress = 0
-//
-//        for (i in imageViews.indices) {
-//            imageViews[i].setImageResource(R.drawable.ic_launcher_background)
-//        }
-//    }
-//
-//    private fun criarListaDeImagens(): List<String> {
-//        return List(imageViews.size / 2) { "prof${it + 1}" } + List(imageViews.size / 2) { "prof${it + 1}" }
-//    }
-//
-//    private fun indoTelaPerdeu() {
-//        val intent = Intent(this, PerdeuActivity::class.java)
-//      startActivity(intent)
-//   }
-//
-//   private fun indoTelaGanhou() {
-//       val intent = Intent(this, GanhouActivity::class.java)
-//       startActivity(intent)
-//    }
-//
-//}
-//
+    private fun atualizarInterface() {
 
-//class MainActivity: AppCompatActivity() {
-//
-//    private lateinit var jogo: Jogo
-//    private lateinit var imageViews: MutableList<ImageView>
-//    private lateinit var tvTentativas: TextView
-//    private lateinit var progressBar1: ProgressBar
-//
-//    override fun onCreate(savedInstanceState: Bundle?) {
-//        super.onCreate(savedInstanceState)
-//        setContentView(R.layout.activity_main)
-//
-//        this.tvTentativas = findViewById(R.id.tvTentativas)
-//        this.progressBar1 = findViewById(R.id.progressBar1)
-//
-//        this.jogo = Jogo()
-//
-//        this.imageViews = mutableListOf(
-//            findViewById(R.id.imageProf1),
-//            findViewById(R.id.imageProf2),
-//            findViewById(R.id.imageProf3),
-//            findViewById(R.id.imageProf4),
-//            findViewById(R.id.imageProf5),
-//            findViewById(R.id.imageProf6),
-//            findViewById(R.id.imageProf7),
-//            findViewById(R.id.imageProf8),
-//            findViewById(R.id.imageProf9),
-//            findViewById(R.id.imageProf10),
-//            findViewById(R.id.imageProf11),
-//            findViewById(R.id.imageProf12),
-//            findViewById(R.id.imageProf13),
-//            findViewById(R.id.imageProf14),
-//            findViewById(R.id.imageProf15),
-//            findViewById(R.id.imageProf16)
-//        )
-//
-//        for (imageView in imageViews) {
-//            imageView.setOnClickListener {
-//                if (podeVirarCarta(imageView)) {
-//                    virarCarta(imageView)
-//                }
-//            }
-//        }
-//        progress()
-//    }
-//    private fun podeVirarCarta(carta: ImageView): Boolean {
-//        return jogo.verificarTentativa(carta.tag as String)
-//    }
-//    private fun virarCarta(carta: ImageView) {
-//        val nomeCarta = carta.tag as String
-//        val imagemResource = getImageResourceByName(nomeCarta)
-//        if (imagemResource != 0) {
-//            carta.setImageResource(imagemResource)
-//        } else {
-//            carta.setImageResource(R.drawable.ic_launcher_background)
-//        }
-//
-//        if (jogo.verificarFimDoJogo()) {
-//            if (jogo.paresEncontrados == 8) {
-//                Toast.makeText(this, "Você ganhou!", Toast.LENGTH_SHORT).show()
-//                this.indoTelaGanhou()
-//            } else {
-//                Toast.makeText(this, "Você perdeu!", Toast.LENGTH_SHORT).show()
-//                this.indoTelaPerdeu()
-//            }
-//            novoJogo()
-//        }
-//    }
-//    private fun progress() {
-//        Thread {
-//            while (this.progressBar1.progress < 100) {
-//                this.progressBar1.progress += 1
-//                Thread.sleep(100)
-//            }
-//            this.progressBar1.visibility = View.INVISIBLE
-//        }.start()
-//    }
-//    private fun indoTelaPerdeu() {
-//        val intent = Intent(this, PerdeuActivity::class.java)
-//      startActivity(intent)
-//   }
-//
-//   private fun indoTelaGanhou() {
-//       val intent = Intent(this, GanhouActivity::class.java)
-//       startActivity(intent)
-//    }
-//
-//    private fun novoJogo() {
-//        this.jogo.novoJogo()
-//        tvTentativas.text = "Tentativas: ${jogo.numeroMaximoTentativas}"
-//        progressBar1.progress = 0
-//
-//        for (imageView in imageViews) {
-//            imageView.setImageResource(R.drawable.ic_launcher_background)
-//        }
-//    }
-//    private fun getImageResourceByName(name: String): Int {
-//        return resources.getIdentifier(name, "drawable", packageName)
-//    }
-//}
-//    private lateinit var jogo: Jogo
-//    private lateinit var imageViews: List<ImageView>
-//    private lateinit var tvTentativas: TextView
-//    private lateinit var progressBar1: ProgressBar
-//    private var primeiraCarta: ImageView? = null
-//    private var segundaCarta: ImageView? = null
-//    override fun onCreate(savedInstanceState: Bundle?) {
-//        super.onCreate(savedInstanceState)
-//        setContentView(R.layout.activity_main)
-//
-//        this.tvTentativas = findViewById(R.id.tvTentativas)
-//        this.progressBar1 = findViewById(R.id.progressBar1)
-//
-//        primeiraCarta?.setOnClickListener { jogo.virarCarta(primeiraCarta!!, "imageProf1") }
-//        segundaCarta?.setOnClickListener { jogo.virarCarta(segundaCarta!!, "imageProf2") }
-//
-//        this.jogo = Jogo()
-//        imageViews = listOf(
-//            findViewById(R.id.imageProf1),
-//            findViewById(R.id.imageProf2),
-//            findViewById(R.id.imageProf3),
-//            findViewById(R.id.imageProf4),
-//            findViewById(R.id.imageProf5),
-//            findViewById(R.id.imageProf6),
-//            findViewById(R.id.imageProf7),
-//            findViewById(R.id.imageProf8)
-//        )
-//
-//        for (imageView in imageViews) {
-//            imageView.setOnClickListener {
-//                if (podeVirarCarta()) {
-//                    jogo.virarCarta(imageView, "imageProf1")
-//                }
-//            }
-//        }
-//        progress()
-//    }
-//    private fun podeVirarCarta(): Boolean {
-//        return segundaCarta == null
-//    }
-//
-//    private fun progress(){
-//        Thread{
-//            while (this.progressBar1.progress < 100){
-//                this.progressBar1.progress += 1
-//                Thread.sleep(100)
-//            }
-//            this.progressBar1.visibility = View.INVISIBLE
-//        }.start()
-//    }
-//    private fun tentativaErrada() {
-//        jogo.registrarTentativaErrada()
-//        val tentativasErradas = jogo.getTentativasErradas()
-//        tvTentativas.text = "Tentativas: $tentativasErradas"
-//    }
-//    private fun indoTelaPerdeu() {
-//        val intent = Intent(this, PerdeuActivity::class.java)
-//        startActivity(intent)
-//    }
-//
-//    private fun indoTelaGanhou() {
-//        val intent = Intent(this, GanhouActivity::class.java)
-//        startActivity(intent)
-//    }
-//    override fun onRestart() {
-//        super.onRestart()
-//        this.jogo.novoJogo()
-//    }
-//}
+        if (this.jogo.verificarFimDoJogo() == "Ganhou!") {
+            this.indoTelaGanhou()
+        } else if (this.jogo.verificarFimDoJogo() == "Perdeu!") {
+            this.indoTelaPerdeu()
+        }
+    }
+    private fun indoTelaPerdeu() {
+        val intent = Intent(this, PerdeuActivity::class.java)
+      startActivity(intent)
+   }
+
+   private fun indoTelaGanhou() {
+       val intent = Intent(this, GanhouActivity::class.java)
+       startActivity(intent)
+   }
+    private fun reiniciarJogo () {
+        this.jogo.novoJogo()
+        this.atualizarInterface()
+        this.memoria.removeAllViews()
+        for (carta in jogo.cartas) {
+            var imageProf = ImageView(this)
+            this.memoria.addView(imageProf)
+            imageProf.layoutParams.height = 150
+            imageProf.layoutParams.width = 150
+            imageProf.setImageResource(carta)
+            imageProf.tag = carta
+        }
+
+        this.progressBar1.progress = 0
+        tvTentativas.setText("Tentativas restantes: ${this.jogo.numeroMaximoTentativas}")
+
+        object : CountDownTimer(10000, 100) {
+            override fun onTick(millisUntilFinished: Long) {
+                progressBar1.progress += 1
+            }
+            override fun onFinish() {
+                virarTodasCartas()
+            }
+        }.start()
+
+    }
+    override fun onRestart() {
+        super.onRestart()
+        this.reiniciarJogo()
+    }
+}
